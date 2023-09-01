da5541b947d841490edc5335f6ce97ba965a2283
Fatma Hussein <fartunhussein346@gmail.com>
2023-09-01 23:26:23 +0300

    successful React-Redux API connection

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import UsersView from "./store/users/usersView";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User List</h1>
+      <UsersView />
+    </div>
+  );
+}
+
+export default App;
